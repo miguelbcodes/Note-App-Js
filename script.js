13197ddcc86cf97ba39c6e7a28f1ddbf62ab0426
Miguel Bertol <miguelbertolcodes@gmail.com>
2022-08-09 23:30:10 -0300

    Update edit feature

--- script.js
+++ script.js
@@ -28,11 +28,13 @@ function showNotes() {
   notesObj.forEach(function(element, index) {
     html += `
           <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Note ${index + 1}</h5>
+            <div id="card-${index}" class="card-body">
+              <h5 class="card-title">Note ${index + 1}</h5> 
               <p class="card-text"> ${element}</p>
               <button id=${index} onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button>
-              <button id=${index} onclick="editNote(${index})" class="btn btn-secondary">Edit Note</button>
+
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-window" data-bs-index="${index + 1}" data-bs-content="${element}">Edit</button>
+              
             </div>
           </div>`
   });
@@ -59,14 +61,33 @@ function deleteNote(index) {
 
 // Editing the note
 function editNote(index) {
+  let content = document.getElementById('message-text').value;
   let notes = localStorage.getItem('notes');
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes)
   };
-  let newText = prompt('Enter the new note:', notesObj[index])
-  notesObj[index] = newText
+  notesObj[index] = content;
   localStorage.setItem('notes', JSON.stringify(notesObj));
   showNotes();
 };
+
+// Display the edit window
+let editWindow = document.getElementById('edit-window')
+editWindow.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  let button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  let content = button.getAttribute('data-bs-content')
+  let title = `Editing: Note ${button.getAttribute('data-bs-index')}`;
+  let id = Number(button.getAttribute('data-bs-index')) - 1;
+  // Update the modal's content.
+  let modalBodyTextArea = editWindow.querySelector('.modal-body textarea');
+  let modalHeaderTitle = editWindow.querySelector('.modal-header h5');
+  let modalFooterEditButton = editWindow.querySelector('.modal-footer #edit-btn');
+
+  modalBodyTextArea.value = content;
+  modalHeaderTitle.textContent = title;
+  modalFooterEditButton.setAttribute('onclick', `editNote(${id})`);
+});
